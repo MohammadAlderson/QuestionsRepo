1205113ffa57b8972a6147fcf04b14065df6aaf1
Mohammad Hooshmand <mohammadhooshmand76@gmail.com>
2020-07-17 20:41:43 +0430

    config for liara

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -3,7 +3,11 @@ const mongoose = require("mongoose");
 mongoose.Promise = global.Promise;
 mongoose.connect(
   process.env.MONGODB_URI || "mongodb://localhost:27017/QuestionsRepo",
-  { useNewUrlParser: true, useUnifiedTopology: true }
+  { useNewUrlParser: true, useUnifiedTopology: true, authSource: "admin" }
 );
+
+mongoose.connection.on("open", function () {
+  console.log("Connected to MongoDB");
+});
 
 module.exports = { mongoose };
